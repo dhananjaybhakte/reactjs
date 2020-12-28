4ebc22e04c98ceda8a5da41a1c8a4edc17ede6cb
Jeetendranayak <72904518+Jeetendranayak@users.noreply.github.com>
2020-12-28 18:38:10 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,1 +1,16 @@
-
+const sonarqubeScanner = require("sonarqube-scanner");
+sonarqubeScanner(
+  {
+    serverUrl: "http://sonarqube:9000",
+    token: "9872d4faf4120ff910b524ed3d6c25616a9146e2",
+    options: {
+      "sonar.sources": "./src",
+      "sonar.exclusions": "**/__tests__/**",
+      "sonar.tests": "./src/__tests__",
+      "sonar.test.inclusions": "./src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts",
+      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+      "sonar.testExecutionReportPaths": "reports/test-report.xml",
+    },
+  },
+  () => {},
+);
